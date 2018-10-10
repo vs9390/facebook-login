144d3355187bcf35c5bcf0907655c2380e8fe869
Vishal Sharma <vs9390@gmail.com>
2018-10-10 17:22:08 +0530

    fixed json parser

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var bodyParser = require('body-parser');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/facebook-app'));
+app.use(bodyParser.json());
 
 app.get('/webhook', function(req,res){
     console.log(JSON.stringify(req.query));
